9ddea7efc708edc9032f935035deb79b8bdb9782
johmcg <jpmcg33@gmail.com>
2024-11-14 18:16:06 -0500

    Fixed Results not Showing

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,31 @@ const resulting = document.getElementById("result");
 const checkAll = () => {
 
 
-let formattedText = textInput.value.toLowerCase().replace(/[.,_\s]/g, "");
+    let formattedText = textInput.value.toLowerCase().replace(/[.,_\s]/g, "");
 
-//Same Back and Forward
-let rev = formattedText.split("").reverse().join("");
+    //Same Back and Forward
+    let rev = formattedText.split("").reverse().join("");
 
-if(formattedText === ''){
-    alert("Please input a value");
+    if (formattedText === '') {
+        alert("Please input a value");
     }
 
 
-else if(resulting.textContent === 'A man, a plan, a canal//. Panama'){
-    resulting.textContent =  `${textInput.value} is a palindrome`;
+    else if (resulting.textContent === 'A man, a plan, a canal//. Panama') {
+        resulting.textContent = `${textInput.value} is a palindrome`;
     }
 
 
-//Single Letter
-else if(formattedText.length === 1){
-    resulting.textContent =  `${textInput.value} is a palindrome`;
+    //Single Letter
+    else if (formattedText.length === 1) {
+        resulting.textContent = `${textInput.value} is a palindrome`;
     }
-else if (rev == formattedText) {
-    resulting.textContent =  `${textInput.value} is a palindrome`;
+    else if (rev == formattedText) {
+        resulting.textContent = `${textInput.value} is a palindrome`;
     }
 
-else{
-    resulting.textContent =  `${textInput.value} is not a palindrome`;
+    else {
+        resulting.textContent = `${textInput.value} is not a palindrome`;
     };
 
 }
