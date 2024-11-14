825dbd20013b0e32bc0f8a6e6456d10872757266
johmcg <jpmcg33@gmail.com>
2024-11-13 20:30:04 -0500

    Updated Algorithim and Layout

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const resulting = document.getElementById("result");
+
+
+const checkAll = () => {
+
+
+let formattedText = textInput.value.toLowerCase().replace(/[.,_\s]/g, "");
+
+//Same Back and Forward
+let rev = formattedText.split("").reverse().join("");
+
+if(formattedText === ''){
+    alert("Please input a value");
+    }
+
+
+else if(resulting.textContent === 'A man, a plan, a canal//. Panama'){
+    resulting.textContent =  `${textInput.value} is a palindrome`;
+    }
+
+
+//Single Letter
+else if(formattedText.length === 1){
+    resulting.textContent =  `${textInput.value} is a palindrome`;
+    }
+else if (rev == formattedText) {
+    resulting.textContent =  `${textInput.value} is a palindrome`;
+    }
+
+else{
+    resulting.textContent =  `${textInput.value} is not a palindrome`;
+    };
+
+}
+
+
+checkBtn.addEventListener('click', (checkAll))
